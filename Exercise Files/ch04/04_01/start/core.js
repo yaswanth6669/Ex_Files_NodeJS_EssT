528925552aefbd04db7b2d0115d8b1b5bb4fd6a8
Kakarlapudi Yaswanth Naga Varma Raju <ykakar533@apac.comcast.com>
2022-06-06 22:29:42 +0530

    core & ask of ch 04

--- Exercise Files/ch04/04_01/start/core.js
+++ Exercise Files/ch04/04_01/start/core.js
@@ -1,8 +1,8 @@
-// const path = require("path");
+const path = require("path");
 const v8 = require("v8");
 
 const dirUploads = path.join(__dirname, "ppp", "files", "uploads");
 
-console.log(path.basename(__dirname));
+console.log(path.basename(__filename));
 console.log(" ^ This name of the current file");
 console.log(v8.getHeapStatistics());
